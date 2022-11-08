f0161f3324804effea85ec13001e5667afb595f7
cyanfhine <yifan03.chen@tum.de>
2022-11-09 00:47:39 +0100

    test1

--- src/pgdp/image/SeamCarving.java
+++ src/pgdp/image/SeamCarving.java
@@ -5,11 +5,32 @@ import java.util.Arrays;
 public class SeamCarving {
 
 	public int computeGradientMagnitude(int v1, int v2) {
-		return (int) Math.pow((v1 - v2), 2);
+		return (int) Math.pow(v1 - v2, 2);
 	}
 
 	public void toGradientMagnitude(int[] picture, int[] gradientMagnitude, int width, int height) {
+		for (int i = 0; i < picture.length; i++) {
 
+			//mehrere if statements für mehr Klarheit
+			if (i >= 0 && i < width) {
+				//pixel aus Zeile 1
+				gradientMagnitude[i] = Integer.MAX_VALUE;
+			} else if (i >= (height - 1) * width && i <= width * height - 1) {
+				//pixel aus unterster Zeile
+				gradientMagnitude[i] = Integer.MAX_VALUE;
+			} else if (i % width == 0) {
+				//pixel aus erster spalte
+				gradientMagnitude[i] = Integer.MAX_VALUE;
+			} else if ((i + width - 1) % width == 0) {
+				//pixel aus letzter spalte
+				gradientMagnitude[i] = Integer.MAX_VALUE;
+			} else {
+				//in x-Richtung gradientMagnitude berechnen
+				gradientMagnitude[i] = computeGradientMagnitude(picture[i - 1], picture[i + 1]);
+				//in y-Richtung gradient Magnitude berechnen und addieren
+				gradientMagnitude[i] = gradientMagnitude[i] + computeGradientMagnitude(picture[i - width], picture[i + width]);
+			}
+		}
 	}
 
 	public void combineMagnitudeWithMask(int[] gradientMagnitude, int[] mask, int width, int height){
