8653c007b2c09d8ea8525445186628a64d15feab
cyanfhine <yifan03.chen@tum.de>
2022-11-08 23:43:43 +0100

    test1

--- src/pgdp/image/SeamCarving.java
+++ src/pgdp/image/SeamCarving.java
@@ -5,25 +5,25 @@ import java.util.Arrays;
 public class SeamCarving {
 
 	public int computeGradientMagnitude(int v1, int v2) {
-		return -1;
+		return (int) Math.pow((v1 - v2), 2);
 	}
 
 	public void toGradientMagnitude(int[] picture, int[] gradientMagnitude, int width, int height) {
+
 	}
 
 	public void combineMagnitudeWithMask(int[] gradientMagnitude, int[] mask, int width, int height){
 
 	}
 
-	public void buildSeams(int[][] seams, long[] seamWeights, int[] gradientMagnitude,
-			int width, int height) {
+	public void buildSeams(int[][] seams, long[] seamWeights, int[] gradientMagnitude, int width, int height) {
 	}
 
 	public void removeSeam(int[] seam, int[] image, int height, int oldWidth) {
 	}
 
 	public int[] shrink(int[] image,int[] mask, int width, int height, int newWidth) {
-		return null;
+		return image;
 	}
 
 }
